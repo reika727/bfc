0ed7dc5486dc2f6c6da6f349eb01698102d231dd
reika727 <reikasuya727@gmail.com>
2021-03-09 04:42:46 +0900

    C標準ライブラリから独立

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS := -Wall -Wextra
 BTA    := bf_to_asm/bf_to_asm
 
 %.out: libbf.o %.o
-	$(LD) -dynamic-linker=/lib64/ld-linux-x86-64.so.2 $^ -lc -o $@
+	$(LD) $^ -o $@
 
 %.o: %.bf
 	$(MAKE) -C $(dir $(BTA)) $(notdir $(BTA))
--- libbf.c
+++ libbf.c
@@ -1,25 +1,8 @@
-#include <stdlib.h>
-
-/* 確保されている領域の先頭へのポインタ */
-char *alloc_ptr;
-/* 確保されている領域のサイズ */
-u_int64_t sz;
-/* Brainfuckのインストラクションポインタ */
-char *bf_ptr;
+#include <sys/types.h>
 
 static int64_t bfc_abs(int64_t n)
 {
     return n >= 0 ? n : -n;
-}
-
-static void bfc_memcpy(void *dst, const void *src, u_int64_t n)
-{
-    u_int64_t i;
-    for (i = 0; i < n; ++i) {
-        *(char*)dst = *(char*)src;
-        dst = (void*)((char*)dst + 1);
-        src = (void*)((char*)src + 1);
-    }
 }
 
 static void bfc_memset(void *buf, char ch, u_int64_t n)
@@ -31,19 +14,51 @@ static void bfc_memset(void *buf, char ch, u_int64_t n)
     }
 }
 
+static void* bfc_sbrk(int64_t inc)
+{
+    static char *current_brk = 0;
+    char *p = current_brk;
+    if (!p) {
+        asm volatile (
+            "mov $12, %%rax\n\t"
+            "mov $0, %%rdi\n\t"
+            "syscall\n\t"
+            "mov %%rax, %0"
+            :"=c"(p)
+        );
+    }
+    asm volatile (
+        "mov $12, %%rax\n\t"
+        "mov %1, %%rdi\n\t"
+        "syscall\n\t"
+        "mov %%rax, %0"
+        :"=c"(current_brk)
+        :"d"(p + inc)
+    );
+    return p;
+}
+
+/* 確保されている領域の先頭へのポインタ */
+char *alloc_ptr;
+/* 確保されている領域のサイズ */
+u_int64_t sz;
+/* Brainfuckのインストラクションポインタ */
+char *bf_ptr;
+
 /* 初期化 */
 void bfc_initialize(void)
 {
     /* Brainfuckは最低でも30000バイトのゼロ埋めされた領域を持ち、
      * インストラクションポインタは初期状態で領域の先頭を指す[要出典] */
-    bf_ptr = alloc_ptr = (char*)malloc(sz = 30000);
-    bfc_memset(alloc_ptr, '\0', 30000);
+    bf_ptr = alloc_ptr = (char*)bfc_sbrk(0);
+    bfc_sbrk(sz = 30000);
+    bfc_memset(alloc_ptr, '\0', sz);
 }
 
 /* 終了処理 */
 void bfc_finalize(void)
 {
-    free(alloc_ptr);
+    bfc_sbrk(-(int64_t)sz);
 }
 
 /* 領域の再確保 */
@@ -55,12 +70,17 @@ void bfc_realloc(void)
     if (0 <= offset && (u_int64_t)offset < sz) {
         return;
     }
-    const u_int64_t margin = (offset < 0 ? bfc_abs(offset) : offset - sz + 1) * 2;
-    char*const new_mem = (char*)malloc(sz + margin);
-    bfc_memcpy(new_mem + (offset < 0 ? margin : 0), alloc_ptr, sz);
-    bfc_memset(new_mem + (offset < 0 ? 0 : sz), '\0', margin);
-    free(alloc_ptr);
-    alloc_ptr = new_mem;
+    const u_int64_t margin = (offset >= 0 ? offset - (int64_t)sz + 1 : bfc_abs(offset)) * 2;
+    bfc_sbrk(margin);
+    if (offset >= 0) {
+        bfc_memset(alloc_ptr + sz, '\0', margin);
+    } else {
+        int64_t i;
+        for (i = sz - 1; i >= 0; --i) {
+            alloc_ptr[i + margin] = alloc_ptr[i];
+        }
+        bfc_memset(alloc_ptr, '\0', margin);
+        bf_ptr += margin;
+    }
     sz += margin;
-    bf_ptr = alloc_ptr + (offset < 0 ? margin : 0) + offset;
 }
