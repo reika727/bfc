1d0b43e482f493929b4c56e65ce6bfc3d71abbfc
reika727 <reikasuya727@gmail.com>
2024-01-12 17:50:59 +0900

    Delete catch

--- bf_to_asm/bf_to_asm.cpp
+++ bf_to_asm/bf_to_asm.cpp
@@ -8,19 +8,16 @@ using namespace std::literals::string_literals;
 
 int main(int argc, char *argv[])
 {
-    try {
-        if (argc != 2) {
-            throw std::invalid_argument("usage: bfc SOURCE_FILE");
-        }
-        std::ifstream input(argv[1]);
-        if (!input) {
-            throw std::invalid_argument("cannot open file "s + argv[1]);
-        }
-        const std::string code{std::istreambuf_iterator<char>(input), std::istreambuf_iterator<char>()};
-        std::cout << brainfuck::translate_into_assembly_language(code) << std::endl;
-        return EXIT_SUCCESS;
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+    if (argc != 2) {
+        std::cerr << "usage: bfc SOURCE_FILE" << std::endl;
         return EXIT_FAILURE;
     }
+    std::ifstream input(argv[1]);
+    if (!input) {
+        std::cerr << "cannot open file "s + argv[1] << std::endl;
+        return EXIT_FAILURE;
+    }
+    const std::string code{std::istreambuf_iterator<char>(input), std::istreambuf_iterator<char>()};
+    std::cout << brainfuck::translate_into_assembly_language(code) << std::endl;
+    return EXIT_SUCCESS;
 }
